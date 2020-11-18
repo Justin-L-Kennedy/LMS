25d8d6666294ab5d61e1d2227b4063481f3123a4
Justin-L-Kennedy <justin.l.kennedy@outlook.com>
2020-11-18 10:29:15 -0600

    Template Conversion complete

--- LMS.UI.MVC/App_Start/BundleConfig.cs
+++ LMS.UI.MVC/App_Start/BundleConfig.cs
@@ -7,24 +7,24 @@ namespace LMS.UI.MVC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/template").Include(
+                      "~/Content/plugins/jQuery/jquery.min.js",
+                      "~/Content/plugins/bootstrap/bootstrap.min.js",
+                      "~/Content/plugins/slick/slick.min.js",
+                      "~/Content/plugins/aos/aos.js",
+                      "~/Content/plugins/venobox/venobox.min.js",
+                      "~/Content/plugins/filterizr/jquery.filterizr.min.js",
+                      "~/Content/plugins/google-map/gmap.js",
+                      "~/Content/js/script.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/plugins/bootstrap/bootstrap.min.css",
+                      "~/Content/plugins/slick/slick.css",
+                      "~/Content/plugins/themify-icons/themify-icons.css",
+                      "~/Content/plugins/animate/animate.css",
+                      "~/Content/plugins/aos/aos.css",
+                      "~/Content/plugins/venobox/venobox.css",
+                      "~/Content/css/style.css"));
         }
     }
 }
